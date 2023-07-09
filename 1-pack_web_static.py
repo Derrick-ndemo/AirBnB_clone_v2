d62caebef32f53f459565951f845b5645c75f56a
Derrick-ndemo <derrickndemo@gmail.com>
2023-07-10 02:18:39 +0300

    Fixed archive to display web_static contents

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
-from datetime import datetime
-from fabric.api import local
 
+from fabric.api import local, env
+import time
+import os
+
+env.hosts = ['localhost']
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    """
+    Compresses the contents of the web_static folder into a .tgz archive.
+
+    Returns:
+        (str): Archive path if the archive has been generated correctly, None otherwise.
+    """
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    try:
+        local("tar -czvf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
         return None
-    return file
